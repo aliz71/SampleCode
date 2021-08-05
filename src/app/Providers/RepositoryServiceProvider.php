634e4e98e7470eb1cf34314ec0fbd887d36d3769
ali zahedi <ali.zahedi1371@gmail.com>
2021-08-05 12:50:22 +0430

    Bind book repository interface to book repository

--- src/app/Providers/RepositoryServiceProvider.php
+++ src/app/Providers/RepositoryServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repositories\BookRepository;
+use App\Repositories\Interfaces\BookRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class RepositoryServiceProvider extends ServiceProvider
@@ -13,7 +15,7 @@ class RepositoryServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(BookRepositoryInterface::class, BookRepository::class);
     }
 
     /**
