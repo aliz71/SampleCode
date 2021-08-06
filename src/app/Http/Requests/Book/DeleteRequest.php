6944accf1e73a84ef5a88d8f5cc83c200403bf25
ali zahedi <ali.zahedi1371@gmail.com>
2021-08-06 14:43:05 +0430

    Create DeleteRequest.php for validating delete book

---
+++ src/app/Http/Requests/Book/DeleteRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests\Book;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class DeleteRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'id' => 'required|exists:books,id'
+        ];
+    }
+
+    protected function prepareForValidation()
+    {
+        $this->merge(['id' => $this->route('book')]);
+    }
+}
