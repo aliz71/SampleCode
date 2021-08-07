f107b6d5c29d2db0a7b42b7ecc70f0e13f30ce9e
ali zahedi <ali.zahedi1371@gmail.com>
2021-08-07 17:22:39 +0430

    Add new methods for downloading CSV and XML files

--- src/app/Http/Controllers/BookController.php
+++ src/app/Http/Controllers/BookController.php
@@ -2,22 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Book\CSVExportRequest;
 use App\Http\Requests\Book\DeleteRequest;
 use App\Http\Requests\Book\StoreRequest;
 use App\Http\Requests\Book\UpdateRequest;
+use App\Http\Requests\Book\XMLExportRequest;
 use App\Repositories\Interfaces\BookRepositoryInterface;
+use App\Services\ExportService;
 use App\Services\ResponseService;
 use Illuminate\Http\JsonResponse;
 
 class BookController extends Controller
 {
-    private $bookRepository, $responseService;
+    private $bookRepository, $responseService, $exportService;
 
     public function __construct(BookRepositoryInterface $bookRepository,
-                                ResponseService $responseService)
+                                ResponseService $responseService,
+                                ExportService $exportService)
     {
         $this->bookRepository = $bookRepository;
         $this->responseService = $responseService;
+        $this->exportService = $exportService;
     }
 
     public function index(): JsonResponse
@@ -42,5 +47,21 @@ class BookController extends Controller
     {
         $this->bookRepository->delete($id);
         return $this->responseService->respond();
+    }
+
+    public function cvsExport(CSVExportRequest $request)
+    {
+        $columns = explode(',', $request->columns);
+        $books = $this->bookRepository->getSelectedColumns($columns);
+        $csvData = $this->exportService->csv('books', $columns, $books->toArray());
+        return $this->responseService->csvDownload($csvData['callback'], $csvData['headers']);
+    }
+
+    public function xmlExport(XMLExportRequest $request)
+    {
+        $columns = explode(',', $request->columns);
+        $books = $this->bookRepository->getSelectedColumns($columns);
+        $xmlData = $this->exportService->xml('books', $books->toArray());
+        return $this->responseService->xmlDownload($xmlData['xmlData'], $xmlData['headers']);
     }
 }
